b31129146143874911a740ff72594c802f687094
Ali Raza <114096901+alirazax@users.noreply.github.com>
2025-01-06 00:37:05 +0500

    .

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",  // This will include all HTML and JS files inside the 'src' folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
